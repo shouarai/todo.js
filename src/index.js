c55261dfee68f7f7085bd6d13073abcc832cc737
shouarai <42760140+shouarai@users.noreply.github.com>
2021-02-03 12:56:24 +0900

    index.jsの修正0203

--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し初期化する
+  const inputtext = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputtext;
+
+  //divのこ子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了のリストに追加
+  document.getElementById("imcomplete-list").appendChild(div);
+
+  //ボタンの完了タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //押された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    //完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    //todo内容のテキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+
+    //liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //divタグの子要素に各要素を追加
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // console.log(addTarget);
+  });
+
+  //ボタンの削除タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  //指定した"imcomplete-list"の子要素にdivを追加
+  document.getElementById("imcomplete-list").appendChild(div);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("imcomplete-list").removeChild(target);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
